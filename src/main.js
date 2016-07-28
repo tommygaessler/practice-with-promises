02a2996f14477afd5a163c8aed67e9b31de1e98c
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-07-28 11:34:39 -0600

    finished promise

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
 console.log('HULK SMASH PATRIARCHY!');
+
+// $.ajax({
+//   method: 'GET',
+//   url: 'http://www.omdbapi.com/?s=batman'
+// }).done(function(results) {
+//   console.log(results);
+// });
+//
+// $.get('http://www.omdbapi.com/?s=batman').done(function(results) {
+//   console.log(results);
+// });
+
+var omdb = 'http://www.omdbapi.com/'
+
+var ajaxCall = $.get(omdb + '?s=batman');
+
+var promise = Promise.resolve(ajaxCall);
+
+promise.then(function(movies) {
+
+    var moviePromise = movies.Search.map(function(movie) {
+    var url = omdb + '?i=' + movie.imdbID;
+    var ajax = $.get(url);
+    return Promise.resolve(ajax);
+  })
+return Promise.all(moviePromise);
+}).then(function(singleMovie) {
+
+  console.log(singleMovie);
+}).catch(function(error) {
+  console.log(error, '!!!');
+});
